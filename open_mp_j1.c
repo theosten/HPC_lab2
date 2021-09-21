cae7c74c76837ec4a6cbcb8d8f004438831259f8
Jakob Hendén <jakob.henden@gmail.com>
2021-09-21 15:16:34 +0200

    testar igen

--- open_mp_j1.c
+++ open_mp_j1.c
@@ -5,71 +5,96 @@
 #include <math.h>
 
 int main (int argc, char *argv[]) {
-	int size = 40;
+	int size = 400000;
 	int dimension = 3;
 	int max_dist = 3465;
-	FILE *fp = fopen("data.txt", "r");
+	FILE *start_fp = fopen("data.txt", "r");
+	FILE *end_fp;
 	int *histogram = (int*) malloc(sizeof(int)*max_dist);
-	short int *points = (short int*) malloc(sizeof(short int)*dimension*size); // <5MiB
-	//float **points = (float**) malloc(sizeof(float*)*size);
-	//for (size_t i = 0, j = 0; i < size; i++, j+=dimension)
-	//	points[i] = cords + j;
-	int length = -1;
+	for (size_t i = 0; i < max_dist; i++)
+		histogram[i] = 0;
+	short int *start_points = (short int*) malloc(sizeof(short int)*dimension*size); // <2.5MiB
+	short int *end_points = (short int*) malloc(sizeof(short int)*dimension*size); // <2.5MiB
+	int length = size*3;
 	float dist;
 	omp_set_num_threads(8);
+	char array[24];
+	int done = 0;
 	clock_t time = clock();
 
-	// Read as char
-	// Read in parallel
-	// double?
-
-	short int number;
-	char array[6];
-	for (size_t i = 0; i < size*dimension; i+=dimension) {
-		if (fread(array, 1, 3, fp) == 3) {
-			fseek(fp, 1, SEEK_CUR);
-			fread(array+3, 1, 3, fp);
-			fseek(fp, 1, SEEK_CUR);
-			number = (array[1]-'0')*10000+(array[2]-'0')*1000+(array[3]-'0')*100
-							 +(array[4]-'0')*10+array[5]-'0';
-			if (array[0] == '-')
-				number *= -1;
-			points[i] = number;
-			fread(array, 1, 3, fp);
-			fseek(fp, 1, SEEK_CUR);
-			fread(array+3, 1, 3, fp);
-			fseek(fp, 1, SEEK_CUR);
-			number = (array[1]-'0')*10000+(array[2]-'0')*1000+(array[3]-'0')*100
-							 +(array[4]-'0')*10+array[5]-'0';
-			if (array[0] == '-')
-				number *= -1;
-			points[i+1] = number;
-			fread(array, 1, 3, fp);
-			fseek(fp, 1, SEEK_CUR);
-			fread(array+3, 1, 3, fp);
-			fseek(fp, 1, SEEK_CUR);
-			number = (array[1]-'0')*10000+(array[2]-'0')*1000+(array[3]-'0')*100
-							 +(array[4]-'0')*10+array[5]-'0';
-			if (array[0] == '-')
-				number *= -1;
-			points[i+2] = number;
-		} else {
-			length = i/3;
-			break;
+	while (length == size*3) {
+		for (size_t i = 0; i < size*dimension; i+=dimension) {
+			if (fread(array, 1, 24, start_fp) == 24) {
+				start_points[i] = (array[1]-'0')*10000+(array[2]-'0')*1000+(array[4]-'0')*100
+								 +(array[5]-'0')*10+array[6]-'0';
+				if (array[0] == '-')
+					start_points[i] *= -1;
+				
+				start_points[i+1] = (array[9]-'0')*10000+(array[10]-'0')*1000+(array[12]-'0')*100
+								 +(array[13]-'0')*10+array[14]-'0';
+				if (array[8] == '-')
+					start_points[i+1] *= -1;
+				
+				start_points[i+2] = (array[17]-'0')*10000+(array[18]-'0')*1000+(array[20]-'0')*100
+								 +(array[21]-'0')*10+array[22]-'0';
+				if (array[16] == '-')
+					start_points[i+2] *= -1;
+			} else {
+				length = i;
+				break;
+			}
+		}
+		for (size_t i = 0; i < length; i+=dimension) {
+			for (size_t j = i+dimension; j < length; j+=dimension) {
+				dist = sqrtf((float)((start_points[i]-start_points[j])*(start_points[i]-
+						 start_points[j])+(start_points[i+1]-start_points[j+1])*(start_points[i+1]
+						 -start_points[j+1])+(start_points[i+2]-start_points[j+2])*(start_points[i+2]
+						 -start_points[j+2]))/1000000);
+				histogram[(int)(dist*100)] += 1; // collisions highly unlikely
+			}
+		}
+		end_fp = start_fp;
+		done = size*3;
+		while (done == size*3) {
+			for (size_t i = 0; i < size*dimension; i+=dimension) {
+				if (fread(array, 1, 24, end_fp) == 24) {
+					end_points[i] = (array[1]-'0')*10000+(array[2]-'0')*1000+(array[4]-'0')*100
+									 +(array[5]-'0')*10+array[6]-'0';
+					if (array[0] == '-')
+						end_points[i] *= -1;
+				
+					end_points[i+1] = (array[9]-'0')*10000+(array[10]-'0')*1000+(array[12]-'0')*100
+									 +(array[13]-'0')*10+array[14]-'0';
+					if (array[8] == '-')
+						end_points[i+1] *= -1;
+				
+					end_points[i+2] = (array[17]-'0')*10000+(array[18]-'0')*1000+(array[20]-'0')*100
+									 +(array[21]-'0')*10+array[22]-'0';
+					if (array[16] == '-')
+						end_points[i+2] *= -1;
+				} else {
+					done = i;
+					break;
+				}
+			}
+			for (size_t i = 0; i < length; i+=dimension) {
+				for (size_t j = 0; j < done; j+=dimension) {
+					dist = sqrtf((float)((start_points[i]-end_points[j])*(start_points[i]-
+							 end_points[j])+(start_points[i+1]-end_points[j+1])*(start_points[i+1]
+							 -end_points[j+1])+(start_points[i+2]-end_points[j+2])*(start_points[i+2]
+							 -end_points[j+2]))/1000000);
+					histogram[(int)(dist*100)] += 1; // collisions highly unlikely
+				}
+			}
 		}
 	}
 
-	for (size_t i = 0; i < length; i++)
-		printf("%f, %f, %f\n", (float)points[i]/1000, (float)points[i+1]/1000, 
-									  (float)points[i+2]/1000);
-	
-
 	/*while (length == -1) {
 		for (size_t i = 0; i < size*dimension; i+=dimension) {
-			if (fscanf(fp, "%f", &points[i]) == 1) {
+			if (fscanf(start_fp, "%f", &start_points[i]) == 1) {
 				// not for loop for efficiency
-				fscanf(fp, "%f", &points[i+1]);
-				fscanf(fp, "%f", &points[i+2]);
+				fscanf(start_fp, "%f", &start_points[i+1]);
+				fscanf(start_fp, "%f", &start_points[i+2]);
 			} else {
 				printf("Hello\n");
 				length = i;
@@ -80,13 +105,14 @@ int main (int argc, char *argv[]) {
 		#pragma omp parallel for
 		for (size_t i = 0; i < length; i+=3) {
 			for (size_t j = i+3; j < length; j+=3) {
-				dist = sqrtf((points[i]-points[j])*(points[i]-points[j])+
-								 (points[i+1]-points[j+1])*(points[i+1]-points[j+1])+
-								 (points[i+2]-points[j+2])*(points[i+2]-points[j+2]));
+				dist = sqrtf((start_points[i]-start_points[j])*(start_points[i]-start_points[j])+
+								 (start_points[i+1]-start_points[j+1])*(start_points[i+1]-start_points[j+1])+
+								 (start_points[i+2]-start_points[j+2])*(start_points[i+2]-start_points[j+2]));
 				histogram[(int)(dist*100)] += 1; // collisions highly unlikely
 			}
 		}
 	}
+*/
 
 	double avg_time = (double)(clock() - time) / CLOCKS_PER_SEC;
 
@@ -94,9 +120,11 @@ int main (int argc, char *argv[]) {
 		if (histogram[i] != 0)
 			printf("%02d.%02d %d\n", i/100, i%100, histogram[i]);
 	printf("Runtime: %f\n", avg_time);
-	*/
-	fclose(fp);
+	
+	fclose(start_fp);
+	//fclose(end_fp);
 	free(histogram);
-	free(points);
+	free(start_points);
+	free(end_points);
 	return 0;
 }
