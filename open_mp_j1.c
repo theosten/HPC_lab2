5e790eb67844a00c41ec940e4ca002b23d2f9652
Jakob Hendén <jakob.henden@gmail.com>
2021-09-22 15:35:30 +0200

    jag vill gå hem

--- open_mp_j1.c
+++ open_mp_j1.c
@@ -3,93 +3,115 @@
 #include <time.h>
 #include <omp.h>
 #include <math.h>
+#include <unistd.h>
 
 int main (int argc, char *argv[]) {
-	int size = 200000;
+	int opt, t;
+	while ((opt = getopt(argc, argv, "t:")) != -1) {
+		switch (opt) {
+			case 't':
+				t = atoi(optarg);
+				break;
+		}
+	}
+	int size = 100000;
 	int dimension = 3;
 	int bpn = 8;
-	int max_dist = 3465;
-	FILE *fp = fopen("data.txt", "r");
+	#define max_dist 3465
+	FILE *fp = fopen("cells", "r");
 	int block_end;
-	int *histogram = (int*) malloc(sizeof(int)*max_dist);
-	for (size_t i = 0; i < max_dist; i++)
-		histogram[i] = 0;
-	short int *start_points = (short int*) malloc(sizeof(short int)*dimension*size);
-	short int *end_points = (short int*) malloc(sizeof(short int)*dimension*size);
+	int histogram[max_dist] = {0} ;
+	float *start_points = (float*) malloc(sizeof(float)*dimension*size);
+	float *end_points = (float*) malloc(sizeof(float)*dimension*size);
 	char *buffer = (char*) malloc(bpn*dimension*size);
 	float dist;
-	omp_set_num_threads(10);
+	omp_set_num_threads(t);
 	int nr_read_start;
 	int nr_read_end;
+	size_t i;
+	size_t j;
 
-	struct timespec start_time;
+/*	struct timespec start_time;
 	struct timespec end_time;
 	double time;
-	timespec_get(&start_time, TIME_UTC);	
+	timespec_get(&start_time, TIME_UTC);*/
 
 	do {
 		nr_read_start = fread(buffer, 1, bpn*size*dimension, fp);
 		#pragma omp parallel for
-		for (size_t i = 0; i < nr_read_start/bpn; i++) {
-			start_points[i] = (buffer[bpn*i+1]-'0')*10000+(buffer[bpn*i+2]-'0')*1000+
-									(buffer[bpn*i+4]-'0')*100+(buffer[bpn*i+5]-'0')*10+buffer[bpn*i+6]-'0';
+		for (i = 0; i < nr_read_start/bpn; i++) {
+			start_points[i] = (buffer[bpn*i+1]-'0')*10.+(buffer[bpn*i+2]-'0')+
+									(buffer[bpn*i+4]-'0')/10.+(buffer[bpn*i+5]-'0')/100.+
+									(buffer[bpn*i+6]-'0')/1000.;
 			if (buffer[bpn*i] == '-')
 				start_points[i] *= -1;
 		}
-		#pragma omp parallel for shared (histogram)
-		for (size_t i = 0; i < nr_read_start/bpn; i+=dimension) {
-			for (size_t j = i+dimension; j < nr_read_start/bpn; j+=dimension) {
-				dist = sqrtf((float)((start_points[i]-start_points[j])*(start_points[i]-
+		#pragma omp parallel
+		{
+		int local_histogram[max_dist] = {0};
+		#pragma omp for private (i, j) schedule (dynamic, 100)
+		for (i = 0; i < nr_read_start/bpn; i+=dimension) {
+			for (j = i+dimension; j < nr_read_start/bpn; j+=dimension) {
+				dist = sqrtf((start_points[i]-start_points[j])*(start_points[i]-
 						 start_points[j])+(start_points[i+1]-start_points[j+1])*(start_points[i+1]
 						 -start_points[j+1])+(start_points[i+2]-start_points[j+2])*(start_points[i+2]
-						 -start_points[j+2]))/1000000);
-				#pragma omp atomic
-				histogram[(int)(dist*100)] += 1; // collisions highly unlikely
+						 -start_points[j+2]));
+				local_histogram[(int)(dist*100)] += 1;
 			}
 		}
+		#pragma omp critical
+		{
+		for (i = 0; i < max_dist; i++)
+			histogram[i] += local_histogram[i];
+		}}
 
 		block_end = ftell(fp);
 
 		do {
 			nr_read_end = fread(buffer, 1, bpn*size*dimension, fp);
 			#pragma omp parallel for
-			for (size_t i = 0; i < nr_read_end/bpn; i++) {
-				end_points[i] = (buffer[bpn*i+1]-'0')*10000+(buffer[bpn*i+2]-'0')*1000+
-									 (buffer[bpn*i+4]-'0')*100+(buffer[bpn*i+5]-'0')*10+
-									  buffer[bpn*i+6]-'0';
+			for (i = 0; i < nr_read_end/bpn; i++) {
+				end_points[i] = (buffer[bpn*i+1]-'0')*10.+(buffer[bpn*i+2]-'0')+
+									 (buffer[bpn*i+4]-'0')/10.+(buffer[bpn*i+5]-'0')/100.+
+									 (buffer[bpn*i+6]-'0')/1000.;
 				if (buffer[bpn*i] == '-')
 					end_points[i] *= -1;
 			}
-			#pragma omp parallel for shared (histogram)
-			for (size_t i = 0; i < nr_read_start/bpn; i+=dimension) {
-				for (size_t j = 0; j < nr_read_end/bpn; j+=dimension) {
-					dist = sqrtf((float)((start_points[i]-end_points[j])*(start_points[i]-
+			#pragma omp parallel
+			{
+			int local_histogram[max_dist] = {0};
+			#pragma omp for private (i, j) schedule (dynamic, 100)
+			for (i = 0; i < nr_read_start/bpn; i+=dimension) {
+				for (j = 0; j < nr_read_end/bpn; j+=dimension) {
+					dist = sqrtf((start_points[i]-end_points[j])*(start_points[i]-
 							 end_points[j])+(start_points[i+1]-end_points[j+1])*(start_points[i+1]
 							 -end_points[j+1])+(start_points[i+2]-end_points[j+2])*(start_points[i+2]
-							 -end_points[j+2]))/1000000);
+							 -end_points[j+2]));
 					#pragma omp atomic
-					histogram[(int)(dist*100)] += 1; // collisions highly unlikely
+					histogram[(int)(dist*100)] += 1;
 				}
 			}
+			#pragma omp critical
+			{
+			for (i = 0; i < max_dist; i++)
+				histogram[i] += local_histogram[i];
+			}}	
 		} while (nr_read_end == size*dimension*bpn);
 
 		fseek(fp, block_end, SEEK_SET);
 	} while (nr_read_start == size*dimension*bpn);
 
-	timespec_get(&end_time, TIME_UTC);
+/*	timespec_get(&end_time, TIME_UTC);
 	time = difftime(end_time.tv_sec, start_time.tv_sec) +
-			 (end_time.tv_nsec - start_time.tv_nsec) / 1000000000.;
+			 (end_time.tv_nsec - start_time.tv_nsec) / 1000000000.;*/
 
-	int sum = 0;
-	for (size_t i = 0; i <= max_dist; i++)
+	for (i = 0; i < max_dist; i++)
 		if (histogram[i] != 0) {
-			sum += histogram[i];
 			printf("%02d.%02d %d\n", i/100, i%100, histogram[i]);
 		}
-	printf("Runtime: %f, %d\n", time, sum);
+	//printf("Runtime: %f\n", time);
 	
 	fclose(fp);
-	free(histogram);
 	free(start_points);
 	free(end_points);
 	return 0;
