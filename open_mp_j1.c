f6d05b4d79b719b0adad35e382e08392fce0f93d
Jakob Hendén <jakob.henden@gmail.com>
2021-09-22 13:06:37 +0200

    varsegoo david

--- open_mp_j1.c
+++ open_mp_j1.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 int main (int argc, char *argv[]) {
-	int size = 4;
+	int size = 200000;
 	int dimension = 3;
 	int bpn = 8;
 	int max_dist = 3465;
@@ -18,7 +18,7 @@ int main (int argc, char *argv[]) {
 	short int *end_points = (short int*) malloc(sizeof(short int)*dimension*size);
 	char *buffer = (char*) malloc(bpn*dimension*size);
 	float dist;
-	omp_set_num_threads(4);
+	omp_set_num_threads(10);
 	int nr_read_start;
 	int nr_read_end;
 	
@@ -26,14 +26,14 @@ int main (int argc, char *argv[]) {
 
 	do {
 		nr_read_start = fread(buffer, 1, bpn*size*dimension, fp);
-		printf("nr_read_start: %d\n", nr_read_start/24);
+		#pragma omp parallel for
 		for (size_t i = 0; i < nr_read_start/bpn; i++) {
 			start_points[i] = (buffer[bpn*i+1]-'0')*10000+(buffer[bpn*i+2]-'0')*1000+
 									(buffer[bpn*i+4]-'0')*100+(buffer[bpn*i+5]-'0')*10+buffer[bpn*i+6]-'0';
 			if (buffer[bpn*i] == '-')
 				start_points[i] *= -1;
 		}
-
+		#pragma omp parallel for
 		for (size_t i = 0; i < nr_read_start/bpn; i+=dimension) {
 			for (size_t j = i+dimension; j < nr_read_start/bpn; j+=dimension) {
 				dist = sqrtf((float)((start_points[i]-start_points[j])*(start_points[i]-
@@ -48,15 +48,15 @@ int main (int argc, char *argv[]) {
 
 		do {
 			nr_read_end = fread(buffer, 1, bpn*size*dimension, fp);
-			printf("nr_read_end: %d\n", nr_read_end/24);
-			for (size_t i = 0; i < nr_read_start/bpn; i++) {
-				start_points[i] = (buffer[bpn*i+1]-'0')*10000+(buffer[bpn*i+2]-'0')*1000+
-										(buffer[bpn*i+4]-'0')*100+(buffer[bpn*i+5]-'0')*10+
-										 buffer[bpn*i+6]-'0';
+			#pragma omp parallel for
+			for (size_t i = 0; i < nr_read_end/bpn; i++) {
+				end_points[i] = (buffer[bpn*i+1]-'0')*10000+(buffer[bpn*i+2]-'0')*1000+
+									 (buffer[bpn*i+4]-'0')*100+(buffer[bpn*i+5]-'0')*10+
+									  buffer[bpn*i+6]-'0';
 				if (buffer[bpn*i] == '-')
-					start_points[i] *= -1;
+					end_points[i] *= -1;
 			}
-
+			#pragma omp parallel for
 			for (size_t i = 0; i < nr_read_start/bpn; i+=dimension) {
 				for (size_t j = 0; j < nr_read_end/bpn; j+=dimension) {
 					dist = sqrtf((float)((start_points[i]-end_points[j])*(start_points[i]-
@@ -73,11 +73,13 @@ int main (int argc, char *argv[]) {
 
 	double avg_time = (double)(clock() - time) / CLOCKS_PER_SEC;
 
+	int sum = 0;
 	for (size_t i = 0; i <= max_dist; i++)
 		if (histogram[i] != 0) {
+			sum += histogram[i];
 			printf("%02d.%02d %d\n", i/100, i%100, histogram[i]);
 		}
-	printf("Runtime: %f\n", avg_time);
+	printf("Runtime: %f, %d\n", avg_time, sum);
 	
 	fclose(fp);
 	free(histogram);
