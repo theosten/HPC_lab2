b71f97f63a6e6a091c2fb5238697a4602aac87d5
Jakob Hendén <jakob.henden@gmail.com>
2021-09-24 11:18:28 +0200

    ehj

--- open_mp_j1.c
+++ open_mp_j1.c
@@ -20,16 +20,17 @@ int main (int argc, char *argv[]) {
 	#define max_dist 3465
 	FILE *fp = fopen("cells", "r");
 	int block_end;
-	int histogram[max_dist] = {0} ;
+	int histogram[max_dist*t];
 	float *start_points = (float*) malloc(sizeof(float)*dimension*size);
 	float *end_points = (float*) malloc(sizeof(float)*dimension*size);
 	char *buffer = (char*) malloc(bpn*dimension*size);
-	float x, y, z;
 	omp_set_num_threads(t);
 	int nr_read_start;
 	int nr_read_end;
 	size_t i;
 	size_t j;
+	for (i = 0; i < max_dist*t; i++)
+		histogram[i] = 0;
 
 	struct timespec start_time;
 	struct timespec end_time;
@@ -38,8 +39,6 @@ int main (int argc, char *argv[]) {
 
 	do {
 		nr_read_start = fread(buffer, 1, bpn*size*dimension, fp)/bpn;
-		printf("Start block: %d\n", nr_read_start);
-		#pragma omp parallel for
 		for (i = 0; i < nr_read_start; i++) {
 			start_points[i] = (buffer[bpn*i+1]-'0')*10.+(buffer[bpn*i+2]-'0')+
 									(buffer[bpn*i+4]-'0')/10.+(buffer[bpn*i+5]-'0')/100.+
@@ -47,23 +46,31 @@ int main (int argc, char *argv[]) {
 			if (buffer[bpn*i] == '-')
 				start_points[i] *= -1;
 		}
-		
-		#pragma omp parallel for reduction (+:histogram)
+		printf("Starting calc\n");
+		#pragma omp parallel shared (histogram)
+		{
+		int thread = omp_get_thread_num()*max_dist;
+		float x, y, z;
+		#pragma omp for
 		for (i = 0; i < nr_read_start; i+=dimension) {
 			for (j = i+dimension; j < nr_read_start; j+=dimension) {
 				x = start_points[i]-start_points[j];
 				y = start_points[i+1]-start_points[j+1];
 				z = start_points[i+2]-start_points[j+2];
-				histogram[(int)(sqrtf(x*x+y*y+z*z)*100)] += 1;
+				histogram[(int)(sqrtf(x*x+y*y+z*z)*100+thread)] += 1;
 			}
 		}
-		
+		}
+		printf("Finished calc\n");
+
 		block_end = ftell(fp);
 
-		do {
+/*		do {
 			nr_read_end = fread(buffer, 1, bpn*size*dimension, fp)/bpn;
-			printf("End block: %d\n", nr_read_end);
-			#pragma omp parallel for
+			#pragma omp parallel
+			{
+			int local_histogram[max_dist];
+			#pragma omp for
 			for (i = 0; i < nr_read_end; i++) {
 				end_points[i] = (buffer[bpn*i+1]-'0')*10.+(buffer[bpn*i+2]-'0')+
 									 (buffer[bpn*i+4]-'0')/10.+(buffer[bpn*i+5]-'0')/100.+
@@ -72,7 +79,7 @@ int main (int argc, char *argv[]) {
 					end_points[i] *= -1;
 			}
 			
-			#pragma omp parallel for reduction (+:histogram)
+			#pragma omp for
 			for (i = 0; i < nr_read_start; i+=dimension) {
 				for (j = 0; j < nr_read_end; j+=dimension) {
 					x = start_points[i]-end_points[j];
@@ -81,7 +88,13 @@ int main (int argc, char *argv[]) {
 					histogram[(int)(sqrtf(x*x+y*y+z*z)*100)] += 1;
 				}
 			}
-		} while (nr_read_end == size*dimension);
+
+			#pragma omp critical
+			{
+			for (i = 0; i < max_dist; i++)
+				histogram[i] += local_histogram[i];
+			}}
+		} while (nr_read_end == size*dimension);*/
 
 		fseek(fp, block_end, SEEK_SET);
 	} while (nr_read_start == size*dimension);
@@ -89,13 +102,19 @@ int main (int argc, char *argv[]) {
 	timespec_get(&end_time, TIME_UTC);
 	time = difftime(end_time.tv_sec, start_time.tv_sec) +
 			 (end_time.tv_nsec - start_time.tv_nsec) / 1000000000.;
-	int sum = 0;
+
+	int merged_hist[max_dist] = {0};
 	for (i = 0; i < max_dist; i++)
-		if (histogram[i] != 0) {
-			sum += histogram[i];
+		for (j = i; j < max_dist*t; j+=max_dist)
+			merged_hist[i] += histogram[j];
+
+	long int sum = 0;
+	for (i = 0; i < max_dist; i++)
+		if (merged_hist[i] != 0) {
+			sum += merged_hist[i];
 			//printf("%02d.%02d %d\n", i/100, i%100, histogram[i]);
 		}
-	printf("Runtime: %f, %d\n", time, sum);
+	printf("Runtime: %f, %ld\n", time, sum);
 	
 	fclose(fp);
 	free(start_points);
